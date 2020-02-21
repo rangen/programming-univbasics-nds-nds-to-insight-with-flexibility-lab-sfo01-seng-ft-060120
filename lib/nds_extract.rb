47f5655558e967e76bf2cff749c8aa10514d461d
Don Mallory <dmm333@gmail.com>
2020-02-21 19:46:03 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -55,7 +55,7 @@ def movies_with_director_key(name, movies_collection)
     collection_with_name << flatten_a_o_a(movie_with_director_name(name, movies_collection[index]))
     index += 1
   end
-
+  collection_with_name
 end
 
 
