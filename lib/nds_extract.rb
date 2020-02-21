171beb9948bf5a3745d25e601c7561671e347b4d
Don Mallory <dmm333@gmail.com>
2020-02-21 20:22:41 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -68,7 +68,7 @@ def movies_with_directors_set(source)
   # Array of Hashes containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
   index = 0
-  with_directors_set = {}
+  with_directors_set = []
 
   while index < source.length
     newHash = {:director_name => source[index][:name], :movies => source[index][:movies]}
